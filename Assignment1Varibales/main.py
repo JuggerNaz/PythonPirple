bf9385d7f5f4bb2b91ba7abeddd9a66f3393e914
JuggerNaz <nazrul.muhaimin@gmail.com>
2020-10-20 23:26:37 +0800

    assignment 1 variables

---
+++ Assignment1Varibales/main.py
@@ -0,0 +1,29 @@
+"""
+    Assignment #1: Variables
+    Author: Nazrul Muhaimin Bin Rahmat
+    Date: 20 Oct 2010
+"""
+
+Artist = "Rossa"
+Title = "Takkan Berpaling Dari-Mu"
+Album = "Rossa"
+Genre = "Pop"
+DurationInSeconds = "230"
+Language = "Malay Indon"
+Country = "Indonesia"
+YearRelease = "2009"
+Composer = "Rossa"
+Producer = "Trinity Optima Production"
+SongWriters = "Aji Mohammad Mizra Ferdinand Hakim"
+
+print(Artist)
+print(Title)
+print(Album)
+print(Genre)
+print(DurationInSeconds)
+print(Language)
+print(Country)
+print(YearRelease)
+print(Composer)
+print(Producer)
+print(SongWriters)

a9508d3ba265bc70590e4c7a7b4974bc63be7adc
JuggerNaz <nazrul.muhaimin@gmail.com>
2020-10-22 23:18:33 +0800

    assignment 2

---
+++ Assignment2Functions/main.py
@@ -0,0 +1,22 @@
+"""
+    Assignment #2: Functions
+    Author: Nazrul Muhaimin Bin Rahmat
+    Date: 22 Oct 2010
+"""
+
+def Artist():
+    return "Rossa"
+
+def Title():
+    return "Takkan Berpaling Dari-Mu"
+
+def Album():
+    return "Rossa"
+
+def isIndonesian(country):
+    return country == "Indonesia"
+
+print(Artist())
+print(Title())
+print(Album())
+print(isIndonesian("Malaysia"))
